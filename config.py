6476f5339a1541d35021e8d265b0a11085425cc4
SaliraVoda <161962941+SaliraVoda@users.noreply.github.com>
2024-04-05 15:06:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Config(object):
     # pyro client config
     API_ID    = os.environ.get("API_ID", "")  # ⚠️ Required
     API_HASH  = os.environ.get("API_HASH", "") # ⚠️ Required
-    BOT_TOKEN = os.environ.get("TOKEN", "") # ⚠️ Required
+    TOKEN = os.environ.get("TOKEN", "") # ⚠️ Required
     START_PIC = os.environ.get("START_PIC", "") # ⚠️ Required
     CHAT = os.environ.get("CHAT", "") # ⚠️ Required
