b95f49b38ca96e167904f7236a9410e622607a81
SaliraVoda <161962941+SaliraVoda@users.noreply.github.com>
2024-04-05 14:55:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
-import re, os, time
+from os import getenv
 
-class Config(object):
-    # pyro client config
-    API_ID    = os.environ.get("API_ID", "")  # ⚠️ Required
-    API_HASH  = os.environ.get("API_HASH", "") # ⚠️ Required
-    TOKEN = os.environ.get("TOKEN", "") # ⚠️ Required
-    CHAT = os.environ.get("CHAT", "") # ⚠️ Required
-    START_PIC    = os.environ.get("START_PIC", "")  # ⚠️ Required
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(getenv("API_ID", 27353035))
+API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
+TOKEN = getenv("TOKEN", None)
+CHAT = int(getenv("CHAT", "-1005822700831"))
+START_PIC = getenv("START_PIC", None)
