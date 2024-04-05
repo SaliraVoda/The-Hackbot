c688dd24ac2fdd28d3f6359566b683e62f2a76cb
ALMIGHTY <93327117+ALMIGHTY-XD@users.noreply.github.com>
2024-04-05 09:21:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
-class Config:
-    API_ID = 
-    API_HASH = ""
-    TOKEN = ""  
-    START_PIC = "" 
-    CHAT = ""    
+import re, os, time
+
+class Config(object):
+    # pyro client config
+    API_ID    = os.environ.get("API_ID", "")  # ⚠️ Required
+    API_HASH  = os.environ.get("API_HASH", "") # ⚠️ Required
+    TOKEN = os.environ.get("TOKEN", "") # ⚠️ Required
+    CHAT = os.environ.get("CHAT", "") # ⚠️ Required
+    START_PIC    = os.environ.get("START_PIC", "")  # ⚠️ Required
